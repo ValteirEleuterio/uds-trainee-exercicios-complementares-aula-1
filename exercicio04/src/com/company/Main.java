9260b7c8ad203df02a938df4b19d81b6234ab3e3
ValteirEleuterio <valteir.ea@gmail.com>
2018-06-14 00:16:34 -0300

    Exercicio 04 -- Verifica se o numero é inteiro ou decimal --

---
+++ exercicio04/src/com/company/Main.java
@@ -0,0 +1,24 @@
+package com.company;
+
+import java.util.Scanner;
+import java.util.function.IntToDoubleFunction;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Digite um numero: ");
+        Float numero = sc.nextFloat();
+
+        System.out.println(verificaInteiro(numero));
+    }
+
+    private static String verificaInteiro(Float numero){
+        Float resultado =  numero % 1.0f;
+        if (resultado == 0.0f)
+            return "O numero é inteiro!";
+        else
+            return "O numero é decimal!";
+    }
+}
